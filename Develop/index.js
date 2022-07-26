d5443902f0c48df20e78ea0a3128cc65cc4277ce
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-26 15:41:03 -0700

    added a write to readme file function so that the user's input can be displayed in the command line. just need to do last todo in index.js file to initialize the app

--- Develop/index.js
+++ Develop/index.js
@@ -54,6 +54,34 @@ const questions = [
 
 ];
 // TODO: Create a function to write README file
-
+function writeToFile(input) {
+    var readMeText = `\n# ${input.title}
+\n##Table of Contents 
+\t[Description]
+\t[Installation]
+\t[Usage]
+\t[License]
+\t[Contributing]
+\t[Testing]
+\t[Questions]\n
+## Description 
+\t ${input.description}
+## Installation 
+\t ${input.installation}
+## Usage
+\t ${input.usage}
+## License
+\t ${input.license}
+## Contributing
+\t ${input.contributions}
+## Testing
+\t ${input.test}
+## Questions 
+  ###Github
+\t ${input.github}
+  ###Email
+\t ${input.email}`
+console.log(readMeText)
+}
 
 // TODO: Create a function to initialize app
