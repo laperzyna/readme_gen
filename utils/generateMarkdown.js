d5ef23499ff74fbdbe7e1c7830dc99d28c6ef136
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-26 16:16:24 -0700

    did the renderLicenseLink function TODO, so added those links to the badges. Also forgot to say I deleted the develop folder cuz I needed to and it was giving me all sorts of issues.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,28 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  //switch case to link the correct badge to the correct license
+  switch(license) {
+    case "MIT":
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "BSD":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "GPL":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+      break;
+    case "Apache":
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.html";
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
