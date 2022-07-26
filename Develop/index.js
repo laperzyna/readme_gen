be16c1f441aadfde5f8f3c5df6c0279776772a01
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-07-26 15:37:53 -0700

    added first ToDo which is just asking the questions now need to do other TODOs and create readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,59 @@
+const inquirer = require("inquirer");
+const path = require('path');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // TODO: Include packages needed for this application
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title: '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project Description: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation Instructions: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Information: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Project License: ",
+        choices: ['MIT', 'Apache', 'GPL']
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Contribution Guidlines: '
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can a user test your software?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Your Github Username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Your Email Address: '
+    },
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
